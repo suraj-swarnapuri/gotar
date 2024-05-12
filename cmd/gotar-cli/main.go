62b25cb3fe89a395d45898a59f7137ba1a2cf2a1
suraj swarnapuri <thriples1@gmail.com>
2024-05-12 18:48:13 +0200

    start over?

--- cmd/gotar-cli/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/suraj-swarnapuri/gotar/cmd/gotar-cli/internal"
-
-func main() {
-	internal.Execute()
-}
--- cmd/gotar/main.go
+++ cmd/gotar/main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"github.com/suraj-swarnapuri/gotar/internal/chord"
-	"github.com/suraj-swarnapuri/gotar/internal/fretboard"
-	"github.com/suraj-swarnapuri/gotar/internal/note"
-	"github.com/suraj-swarnapuri/gotar/internal/scale"
-)
-
 func main() {
-	// ...
-	board := fretboard.NewFretboard()
-
-	sc := scale.NewMajorScale(note.C)
-	chrd := chord.NewDiminishedChord(sc)
-
-	board.Display(chrd, false) // base default board
 
 }
